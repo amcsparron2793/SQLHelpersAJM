86f3d9d67926b33c346c10451df8c726d5f3bf35
Andrew McSparron <Amcsparron@albanyny.gov>
2025-08-28 14:29:31 -0400

    Refactor logging setup to improve validation and flexibility
    Added detailed docstrings for private methods to enhance clarity and maintainability. Refactored `_setup_logger` and `_get_bcl` methods to include more robust validation for keyword arguments and improve configurability, ensuring clearer error handling and behavior.

--- SQLHelpersAJM/__init__.py
+++ SQLHelpersAJM/__init__.py
@@ -15,6 +15,14 @@ class _SharedLogger:
 
     @staticmethod
     def _validate_bcl(**kwargs):
+        """
+        Validates the 'basic_config_level' (bcl) input passed through the keyword arguments.
+
+        :param kwargs: Arbitrary keyword arguments containing potential 'basic_config_level'.
+        :type kwargs: dict
+        :return: The value of 'basic_config_level' if valid; False otherwise.
+        :rtype: str or int or False
+        """
         bcl = kwargs.get('basic_config_level')
         if (bcl in logging.getLevelNamesMapping().keys()
                 or bcl in logging.getLevelNamesMapping().values()):
@@ -22,6 +30,13 @@ class _SharedLogger:
         return False
 
     def _get_bcl(self, **kwargs):
+        """
+        :param kwargs: Keyword arguments that may contain 'logger' for logging and 'basic_config_level'
+            for specifying a configuration level.
+        :type kwargs: dict
+        :return: The validated or default basic configuration level (bcl).
+        :rtype: Any
+        """
         bcl = None
         logger: Optional[Logger] = kwargs.get('logger', None)
         if kwargs.get('basic_config_level'):
@@ -34,15 +49,34 @@ class _SharedLogger:
 
     def _setup_logger(self, **kwargs) -> Logger:
         """
-        Sets up and returns a logger for the class.
-
+        :param kwargs: A dictionary of optional keyword arguments.
+                        - 'logger': An instance of a Logger. If provided, this logger will be used.
+                        - 'logger_name_to_get': A string representing the name of the logger to be retrieved.
+                            Cannot be used together with 'logger'.
+                        - 'skip_basic_config': A boolean indicating whether to skip basic configuration for the logger.
+                            Default is False.
+        :type kwargs: dict
         :return: Configured logger instance.
-        :rtype: logging.Logger
+        :rtype: Logger
+        :raises ValueError: If both 'logger' and 'logger_name_to_get' are provided in kwargs.
         """
-        logger = getLogger(self.__class__.__name__)
-        if not logger.hasHandlers():
-            bcl = self._get_bcl(logger=logger, **kwargs)
-            basicConfig(level=bcl)
+        def _eval_kwargs():
+            if kwargs.get('logger') and kwargs.get('logger_name_to_get'):
+                raise ValueError("Cannot specify both logger and logger_name_to_get.")
+
+            logger_name_to_get = kwargs.get('logger_name_to_get', self.__class__.__name__)
+            lg: Logger = kwargs.get('logger', getLogger(logger_name_to_get))
+
+            sbc = kwargs.get('skip_basic_config', False)
+            return lg, sbc
+
+        logger, skip_basic_config = _eval_kwargs()
+
+        if not skip_basic_config:
+            if not logger.hasHandlers():
+                # noinspection PyTypeChecker
+                bcl = self._get_bcl(logger=logger, **kwargs)
+                basicConfig(level=bcl)
         return logger
 
 
